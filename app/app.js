a377bf3f373dbd08ac614c7bbd03be2a841e185e
milan <milanko.cerny@outlook.com>
2015-05-05 21:40:22 +0200

    [PU] full routing

--- app/app.js
+++ app/app.js
@@ -5,19 +5,35 @@ var PUApp = angular.module('PUApp', [
 PUApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/', {
-        templateUrl: 'login/views/index.html',
-        controller: 'loginMainController'
+	  when('/views/home', {
+        templateUrl: 'views/view.html',
+        controller: 'noopController'
       }).
-	when('/partials', {
-        templateUrl: 'partials/view.html',
+	  when('/views/add-article', {
+        templateUrl: 'views/add-article.html',
+        controller: 'noopController'
+      }).
+	  when('/views/add-event', {
+        templateUrl: 'views/add-event.html',
+        controller: 'noopController'
+      }).
+	  when('/views/my-article', {
+        templateUrl: 'views/my-article.html',
+        controller: 'noopController'
+      }).
+	  when('/views/peoples', {
+        templateUrl: 'views/peoples.html',
+        controller: 'noopController'
+      }).
+	  when('/views/upload', {
+        templateUrl: 'views/upload.html',
         controller: 'noopController'
       }).
       otherwise({
-        redirectTo: '/login'
+        redirectTo: '/views/home'
       });
   }
 ]);
 
-PUApp.controller('loginMainController', loginMainController);
+//PUApp.controller('loginMainController', loginMainController);
 PUApp.controller('noopController', ['$scope', function ($scope) {}]);
